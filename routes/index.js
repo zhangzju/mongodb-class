7a8b07827deabc88f291fe1b36f71176574839ea
zhangzju <zhangway19921221@163.com>
2016-10-30 20:08:50 +0800

    add footer

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mongogo' });
+});
+
+router.get('/document', function(req, res, next) {
+  res.render('document', { title: 'Documents' });
+});
+
+router.get('/database', function(req, res, next) {
+  res.render('database', { title: 'Database' });
 });
 
 module.exports = router;
